3bb26d38571791b9cd76fae94d2985828c1e4bdd
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2024-02-20 07:43:42 -0300

    set User method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'Mario', age: 34 });
 
 user.fetch();
 
 setTimeout(() => {
   console.log(user);
 }, 4000);
+
+// user.set({ name: 'Juliano', age: 24 });
+user.save();
