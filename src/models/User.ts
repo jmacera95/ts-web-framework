8e554a9b3b5c3d90749e4ea272d316d0e9e247e0
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2024-03-02 19:26:44 -0300

    finish implementing passthrough methods

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { Attributes } from './Attributes';
+import { AxiosResponse } from 'axios';
 
 export interface UserProps {
   id?: number;
@@ -29,5 +30,38 @@ export class User {
 
   get trigger() {
     return this.events.trigger;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch data without an id.');
+    }
+
+    this.sync
+      .fetch(id)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
   }
 }
