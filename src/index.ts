4d135180b8423e942cb2903acd7fa5a791a759b0
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2024-04-11 13:03:27 -0300

    first version of view class and views to actually show users

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { User } from './models/User';
+import { UserEdit } from './views/UserEdit';
 import { UserForm } from './views/UserForm';
 
 const user = User.buildUser({ name: 'Juan Carlos', age: 33 });
 const root = document.getElementById('root');
 
 if (root) {
-  const userFormm = new UserForm(root, user);
-
-  userFormm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit);
 } else {
   throw new Error('root element not found.');
 }
